81bc0c57da4d83b8017f2a75181102cd75d35ba4
rasha-sawan18 <101220639@mu.edu.lb>
2025-10-06 00:19:09 +0300

    feat: add messaging component

--- cards/script.js
+++
@@ -1,6 +0,0 @@
-// Log social media redirects
-document.querySelectorAll('.footer a').forEach(link => {
-  link.addEventListener('click', () => {
-    console.log(`Redirecting to: ${link.href}`);
-  });
-});
